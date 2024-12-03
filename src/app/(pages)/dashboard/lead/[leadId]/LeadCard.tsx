2f438617ecab01d1de9aa96b97727b8717158dde
Marco <dev.marcotullio@gmail.com>
2024-12-03 16:00:16 -0300

    persistencia no redux [PATCH]

--- src/app/(pages)/dashboard/lead/[leadId]/LeadCard.tsx
+++ src/app/(pages)/dashboard/lead/[leadId]/LeadCard.tsx
@@ -12,6 +12,14 @@ interface LeadCardProps {
 const LeadCard: React.FC<LeadCardProps> = ({ lead, columns }) => {
     const dispatch = useAppDispatch();
 
+    // Função para encontrar a coluna correspondente ao estado atual do lead
+    const getCurrentColumnId = () => {
+        const currentColumn = columns.find(
+            (column) => column.title.toLowerCase() === lead.pipeline_stage?.toLowerCase()
+        );
+        return currentColumn?.id || ''; // Retorna o `id` da coluna ou uma string vazia
+    };
+
     const handleStatusChange = (newId: string) => {
         const selectedColumn = columns.find((column) => column.id === newId);
         if (!selectedColumn) return;
@@ -29,7 +37,7 @@ const LeadCard: React.FC<LeadCardProps> = ({ lead, columns }) => {
             </div>
             <select
                 className={styles.statusDropdown}
-                value={lead.pipeline_stage?.toLowerCase() || ''} // Atualizado para refletir o estado correto
+                value={getCurrentColumnId()} // Alinha o `value` do `select` com o `id` da coluna atual
                 onChange={(e) => handleStatusChange(e.target.value)}
             >
                 {columns.map((column) => (
