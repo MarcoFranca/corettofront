8b516848c5c839a61f9c5ac430e0698bae1a9ff5
Marco <dev.marcotullio@gmail.com>
2025-01-14 17:29:20 -0300

    remoção do todoSlice [DELL]

--- src/middleware/logoutMiddleware.ts
+++ src/middleware/logoutMiddleware.ts
@@ -3,14 +3,12 @@ import { logout } from '@/store/slices/authSlice';
 import { resetClientes } from '@/store/slices/clientesSlice';
 import { resetLeads } from '@/store/slices/leadsSlice';
 import { resetAgenda } from '@/store/slices/meetingSlice';
-import { resetTodos } from '@/store/slices/todoSlice';
 
 const logoutMiddleware: Middleware = ({ dispatch }) => (next) => (action) => {
     if (logout.match(action)) {
         dispatch(resetClientes());
         dispatch(resetLeads());
         dispatch(resetAgenda());
-        dispatch(resetTodos());
     }
     return next(action);
 };
