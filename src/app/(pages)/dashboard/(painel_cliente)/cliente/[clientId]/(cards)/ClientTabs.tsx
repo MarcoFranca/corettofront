607816d11151ede05f8accef153fc975d425cba6
MarcoTullio <dev.marcotullio@gmail.com>
2025-04-01 23:19:32 -0300

    Ajustes nos nomes dos valores de money [PATCH]

--- src/app/(pages)/dashboard/(painel_cliente)/cliente/[clientId]/(cards)/ClientTabs.tsx
+++ src/app/(pages)/dashboard/(painel_cliente)/cliente/[clientId]/(cards)/ClientTabs.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Tab, Tabs, TabList, TabPanel} from "react-tabs";
 import styles from '../(CustomerComponents)/ClientProfile.module.css';
 import HealthInfoCard from "@/app/(pages)/dashboard/(painel_cliente)/cliente/[clientId]/(cards)/HealthInfoCard";
-import ApolicesTable from "@/app/components/cliente/apoliceClient/ApolicesTable";
+import ApolicesTable from "../apolice/(component)/ApoliceTable";
 import OpportunitiesTable from "@/app/(pages)/dashboard/(painel_cliente)/cliente/[clientId]/reuniao/OpportunitiesTable";
 import Card from "@/app/components/common/Card";
 import {Bar} from "react-chartjs-2";
@@ -21,8 +21,48 @@ import {
     FaFolderOpen,
 } from "react-icons/fa";
 import { MdInsights, MdReportProblem } from "react-icons/md";
+import {
+    ApoliceConsorcio,
+    ApoliceDetalhada,
+    ApolicePlanoSaude,
+    ApolicePrevidencia,
+    ApolicesAgrupadas,
+    ApoliceSeguroVida
+} from "@/types/ApolicesInterface";
+import {useParams} from "next/navigation";
+import api from "@/app/api/axios";
 
 const ClientTabs: React.FC<ClientTabsProps> = ({ cliente, financeData }) => {
+    const { clientId } = useParams();
+    const [apolices, setApolices] = useState<ApoliceDetalhada[]>([]);
+    const [tipoFiltro, setTipoFiltro] = useState('');
+    const [statusFiltro, setStatusFiltro] = useState('');
+    const [visaoGeral, setVisaoGeral] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+
+    useEffect(() => {
+        fetchApolices();
+    }, [tipoFiltro, statusFiltro, visaoGeral, clientId]);
+
+    const fetchApolices = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await api.get<ApoliceDetalhada[]>(
+                `/apolices/?cliente=${clientId}&tipo=${tipoFiltro}&status=${statusFiltro}&visao_geral=${visaoGeral}`
+            );
+            setApolices(response.data);
+        } catch (error) {
+            console.error("Erro ao buscar apólices:", error);
+            setError("Não foi possível carregar as apólices.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <Tabs>
             <TabList className={styles.tabList}>
@@ -50,8 +90,11 @@ const ClientTabs: React.FC<ClientTabsProps> = ({ cliente, financeData }) => {
                 <HealthInfoCard cliente={cliente} />
             </TabPanel>
             <TabPanel>
-                <ApolicesTable  />
-            </TabPanel>
+                <ApolicesTable
+                    setApolices={setApolices}
+                    apolices={apolices}
+                    onEdit={() => {}} // função vazia só pra satisfazer o tipo
+                />            </TabPanel>
             <TabPanel>
                 <Card title="Informações Financeiras">
                     <Bar data={financeData} />
