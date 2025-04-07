6c73dc6c809846b96ac9e773be32ff58e126023a
MarcoTullio <dev.marcotullio@gmail.com>
2025-04-07 11:22:29 -0300

    Removemos slice  [UPDATE]

--- src/middleware/logoutMiddleware.ts
+++ src/middleware/logoutMiddleware.ts
@@ -1,13 +1,11 @@
 import { Middleware } from '@reduxjs/toolkit';
 import { logout } from '@/store/slices/authSlice';
 import { resetClientes } from '@/store/slices/clientesSlice';
-import { resetLeads } from '@/store/slices/leadsSlice';
 import { resetAgenda } from '@/store/slices/meetingSlice';
 
 const logoutMiddleware: Middleware = ({ dispatch }) => (next) => (action) => {
     if (logout.match(action)) {
         dispatch(resetClientes());
-        dispatch(resetLeads());
         dispatch(resetAgenda());
     }
     return next(action);
